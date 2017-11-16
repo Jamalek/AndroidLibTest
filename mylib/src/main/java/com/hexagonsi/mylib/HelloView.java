aec962b899c6db7aba1a8d8300afa64041d63e5c
Jamalek <2janmalek@gmail.com>
2017-11-16 11:38:02 +0100

    init

--- mylib/src/main/java/com/hexagonsi/mylib/HelloView.java
+++ mylib/src/main/java/com/hexagonsi/mylib/HelloView.java
@@ -2,14 +2,15 @@ package com.hexagonsi.mylib;
 
 import android.content.Context;
 import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
 
 /**
  * Created by jmalek on 16.11.2017.
  */
 
 public class HelloView extends AppCompatTextView {
-    public HelloView(Context context) {
-        super(context);
+    public HelloView(Context context, AttributeSet attrs) {
+        super(context, attrs);
         setText("Hello");
     }
 }
